d9e0669037dfaa277eab5212bd38df6b655829d7
Dawson Seibold <dawsonseibold@gmail.com>
2025-11-30 13:00:59 -0700

    Login Page

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginButton = document.getElementById("loginButton");
+
+    if (loginButton) {
+        loginButton.addEventListener("click", function () {
+            window.location.href = "dashboard.html";
+        });
+    }
+});
